2fa099ceef3bcf3274426dfb288d9aef833c0a97
SkizzyCodes <77406156+SkizzyCodes@users.noreply.github.com>
2022-01-16 15:46:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord, os, keep_alive, asyncio, datetime, pytz
+from discord.ext import tasks, commands
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+@client.event
+async def on_connect():
+  await client.change_presence(activity = discord.Streaming(name = "24/7 Online", url = "https://www.twitch.tv/ninja"))
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
